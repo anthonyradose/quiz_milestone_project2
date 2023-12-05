1dbf3ab80d65d142561b9f832d94ee19165f55d3
anthonyradose <asradose@gmail.com>
2023-12-05 12:41:25 +0000

    Add initial structure & functionality of displayQuestion function

--- quiz.js
+++ quiz.js
@@ -1,14 +1,27 @@
-
+let questions;
+let currentQuestionIndex = 0;
 function startGame() {
     const fetchQuestions = () => {
         fetch("https://opentdb.com/api.php?amount=10")
             .then((response) => response.json())
             .then((data) => {
-                let questions = data.results;
+                questions = data.results;
                 console.log(questions);
+                displayQuestion();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                console.error("Failed to load questions. Retrying...");
             });
     };
     fetchQuestions();
 }
 
 startButton.addEventListener("click", startGame);
+
+function displayQuestion() {
+    const questionElement = document.createElement("div");
+    questionElement.innerHTML = `<p>${questions[currentQuestionIndex].question}</p>`;
+    quizContainer.innerHTML = "";
+    quizContainer.appendChild(questionElement);
+}
