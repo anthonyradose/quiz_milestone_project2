e66aef8cfb0eecac5d6c252d7c9ea291d1eb4679
anthonyradose <asradose@gmail.com>
2023-12-05 11:41:50 +0000

    Implement initial API call in the startGame function

--- quiz.js
+++ quiz.js
@@ -1,6 +1,14 @@
 
 function startGame() {
-    console.log('HELLO, YOU HAVE STARTED THE GAME!');
+    const fetchQuestions = () => {
+        fetch("https://opentdb.com/api.php?amount=10")
+            .then((response) => response.json())
+            .then((data) => {
+                let questions = data.results;
+                console.log(questions);
+            });
+    };
+    fetchQuestions();
 }
 
 startButton.addEventListener("click", startGame);
