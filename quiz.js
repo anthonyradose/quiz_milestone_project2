d937efe86125fb9d2f504971d92d85c4d64423ee
anthonyradose <asradose@gmail.com>
2023-12-09 16:20:49 +0000

    Improve structure and separation of concerns in the startGame and fetchQuestions functions.

--- quiz.js
+++ quiz.js
@@ -1,23 +1,47 @@
 let questions;
 let currentQuestionIndex = 0;
+let userScore = 0;
+let isAnsweringAllowed = false;
+const quizContainer = document.getElementById("quizContainer");
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startGame);
 function startGame() {
-    const fetchQuestions = () => {
-        fetch("https://opentdb.com/api.php?amount=10")
-            .then((response) => response.json())
-            .then((data) => {
-                questions = data.results;
-                console.log(questions);
-                displayQuestion();
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-                console.error("Failed to load questions. Retrying...");
-            });
-    };
-    fetchQuestions();
+  console.log("startGame function is being executed.");
+
+  // Check if the game is already in progress
+  if (isAnsweringAllowed) {
+    // If the game is in progress, reset the game state
+    currentQuestionIndex = 0;
+    questions = null;
+        userScore = 0;
+    isAnsweringAllowed = false;
+  }
+
+  // Construct the API URL
+  let apiUrl = "https://opentdb.com/api.php?amount=10";
+
+  fetchQuestions(apiUrl);
 }
 
-startButton.addEventListener("click", startGame);
+const fetchQuestions = (apiUrl) => {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      questions = data.results;
+      if (questions && questions.length > 0) {
+        isAnsweringAllowed = true;
+        displayQuestion();
+      } else {
+        console.error("No questions loaded.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      console.error("Failed to load questions. Retrying...");
+    });
+};
+
 
 function displayQuestion() {
     const questionElement = document.createElement("div");
