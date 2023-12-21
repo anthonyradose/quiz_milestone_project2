c09855e255298451cec7d6636d97b4e0ff89d480
Anthony Radose <asradose@gmail.com>
2023-12-21 11:13:22 +0000

    Restore original fetchQuestions func and remove fetchQuestions mock.

--- quiz.js
+++ quiz.js
@@ -117,7 +117,7 @@ function initializeGameState() {
   categorySelect.disabled = false;
   difficultySelect.disabled = false;
   playAgainButton.style.display = "none";
-quitButton.style.display="none"
+  quitButton.style.display = "none";
   quizContainer.innerHTML = "";
   quizContainer.style.display = "none";
 }
@@ -155,114 +155,40 @@ function startGame() {
     let apiUrl = "https://opentdb.com/api.php?amount=10";
 
     fetchQuestions(apiUrl);
-    
   }
-
-  
-
-
 }
-// const fetchQuestions = (apiUrl) => {
-//   fetch(apiUrl)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       questions = data.results;
-//       if (questions && questions.length > 0) {
-//         isAnsweringAllowed = true; // Set answering flag to true after questions are loaded
-//         displayQuestion();
-//       } else {
-//         console.error("No questions loaded.");
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//       console.error("Failed to load questions. Retrying...");
-//     })
-//     .finally(() => {
-//       loader.style.display = "none";
-//       const answerList = document.getElementById("answerList");
-//       if (answerList) {
-//         answerList.style.pointerEvents = "auto"; // Enable answer options after fetch (success or failure)
-//       }
-//     });
-// };
-
-const fetchQuestions = (apiUrl, timeout = 1000) => {
-  console.log("Fetching questions from:", apiUrl);
-
-  // Create a promise that rejects after the specified timeout
-  const timeoutPromise = new Promise((_, reject) => {
-    setTimeout(() => reject(new Error("Timeout exceeded")), timeout);
-  });
-
-  // Combine the fetch request with the timeout promise
-  Promise.race([fetch(apiUrl), timeoutPromise])
-    .then((response) => {
-      // Check if the response is from the API or the timeout
-      if (response instanceof Response) {
-        console.log("Response status:", response.status);
-        return response.json();
-      } else {
-        throw new Error("API request timed out");
-      }
-    })
+const fetchQuestions = (apiUrl) => {
+  fetch(apiUrl)
+    .then((response) => response.json())
     .then((data) => {
-      console.log("Received data:", data);
       questions = data.results;
       if (questions && questions.length > 0) {
         isAnsweringAllowed = true; // Set answering flag to true after questions are loaded
         displayQuestion();
       } else {
-        console.error("No questions loaded. Using mock data.");
-        // Mock Data for when API is down
-        const mockData = [
-          {
-            category: "General Knowledge",
-            difficulty: "Medium",
-            question: `What is the capital of France<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g data-name="Layer 2"><circle cx="256" cy="256" r="256" fill="#ffea00" opacity="1" data-original="#ff2147" class=""></circle><g fill="#fff"><path d="M252.94 395.94h-25.38c-5.74 0-9.58-3.7-9.6-9.42 2.1-65.89-11.87-60.86 35.69-60.59 45.7-.33 32.19-4.8 34.29 59.78 0 6.71-3.51 10.23-10.17 10.24zM251.9 312c-8.28 0-16.56-.06-24.84 0-6.76.43-8.08-7.28-9-12.35-2.35-21.88 6.75-38.87 22.85-52.87 11.3-10.81 26.76-15.33 38.72-25.09 11.14-9.61 10-25.83-1.87-34.37-12.92-9.26-33.48-10.23-46.73-1.66-9.37 6.58-16.28 16.24-23.7 24.87-4.38 5.21-8.71 5.77-14.12 1.67q-17.05-12.92-34-25.93c-4.78-3.66-5.41-7.9-2.29-13 31.3-57.2 110.88-75.8 163.08-36 35 22.77 50.45 73.95 23.25 108.46-10.63 15-28.66 21.56-42.77 32.52C280.56 295 293 310.76 276.75 312H251.9z" fill="#ffffff" opacity="1" data-original="#ffffff"></path></g></g></g></svg>`,
-            correct_answer: "B. Paris",
-            incorrect_answers: ["C. Berlin", "D. Madrid", "A. Rome"],
-          },
-          // Add more mock questions as needed
-        ];
-        console.log("Using mock data:", mockData);
-        questions = mockData;
-        isAnsweringAllowed = true;
-        displayQuestion();
+        console.error("No questions loaded.");
       }
     })
     .catch((error) => {
       console.error("Error:", error);
+      if (error.response) {
+        console.error("HTTP Status Code:", error.response.status);
+        quizSection.innerHTML = `<p class="error-message">HTTP Status Code: ${error.response.status}</p>`;
+      }
       console.error("Failed to load questions. Retrying...");
-      // Optionally, you can use mock data for testing
-      const mockData = [
-        {
-          category: "General Knowledge",
-          difficulty: "Medium",
-          question: `What is the capital of France <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g data-name="Layer 2"><circle cx="256" cy="256" r="256" fill="#ffea00" opacity="1" data-original="#ff2147" class=""></circle><g fill="#fff"><path d="M252.94 395.94h-25.38c-5.74 0-9.58-3.7-9.6-9.42 2.1-65.89-11.87-60.86 35.69-60.59 45.7-.33 32.19-4.8 34.29 59.78 0 6.71-3.51 10.23-10.17 10.24zM251.9 312c-8.28 0-16.56-.06-24.84 0-6.76.43-8.08-7.28-9-12.35-2.35-21.88 6.75-38.87 22.85-52.87 11.3-10.81 26.76-15.33 38.72-25.09 11.14-9.61 10-25.83-1.87-34.37-12.92-9.26-33.48-10.23-46.73-1.66-9.37 6.58-16.28 16.24-23.7 24.87-4.38 5.21-8.71 5.77-14.12 1.67q-17.05-12.92-34-25.93c-4.78-3.66-5.41-7.9-2.29-13 31.3-57.2 110.88-75.8 163.08-36 35 22.77 50.45 73.95 23.25 108.46-10.63 15-28.66 21.56-42.77 32.52C280.56 295 293 310.76 276.75 312H251.9z" fill="#ffffff" opacity="1" data-original="#ffffff"></path></g></g></g></svg>`,
-          correct_answer: "Paris",
-          incorrect_answers: ["Berlin", "Madrid", "Rome"],
-        },
-        // Add more mock questions as needed
-      ];
-      console.log("Using mock data:", mockData);
-      questions = mockData;
-      isAnsweringAllowed = true;
-      displayQuestion();
     })
     .finally(() => {
       loader.style.display = "none";
       const answerList = document.getElementById("answerList");
       if (answerList) {
-        answerList
-        .style.pointerEvents = "auto"; // Enable answer options after fetch (success or failure)
+        answerList.style.pointerEvents = "auto"; // Enable answer options after fetch (success or failure)
       }
     });
 };
 
 function displayQuestion() {
   quitButton.style.display = "block";
-  progressBar.style.display = "block"
+  progressBar.style.display = "block";
   if (isAnsweringAllowed) {
     // Check if answering is allowed before displaying the question
     const question = questions[currentQuestionIndex];
@@ -293,7 +219,6 @@ function displayQuestion() {
     const answerList = document.getElementById("answerList");
     answerList.addEventListener("click", handleAnswer);
     progressBar.value = currentQuestionIndex;
-    
   }
 }
 
@@ -333,13 +258,10 @@ function handleAnswer(event) {
     if (isCorrect) {
       userScore++;
     }
-    
   }
-  
 }
 
 function displayFeedback(message) {
-  
   const feedbackElement = document.createElement("div");
   feedbackElement.id = "feedbackDiv";
 
@@ -360,7 +282,6 @@ function displayFeedback(message) {
     currentQuestionIndex++;
     if (currentQuestionIndex < 1) {
       displayQuestion();
-
     } else {
       endGame();
     }
